72ab95507802dd786f919f508ee8ece4d03ea5bb
thegrayhulk <thegrayhulk@googlemail.com>
2020-03-28 21:23:58 +0100

    Build aggregate.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HopkinsDataService } from '../domain/hopkins-data.service';
 import { CaseNumbers } from '../domain/caseNumbers';
-import { Observable, from } from 'rxjs';
+import { Observable, from, of, zip, concat } from 'rxjs';
 import * as moment from 'moment';
-import { groupBy } from 'rxjs/operators';
+import { groupBy, mergeAll, scan, flatMap, single, first, mergeMap, map, toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,15 +12,27 @@ import { groupBy } from 'rxjs/operators';
 })
 export class DashboardComponent implements OnInit {
 
-  public CaseNumbers: Observable<Array<CaseNumbers>>;
+  public CaseNumbers: Array<CaseNumbers>;
 
   constructor(private hopkinsService: HopkinsDataService) { }
 
   ngOnInit(): void {
 
-    this.CaseNumbers = this.hopkinsService.getByDay(moment().subtract(1, 'day'));
-    this.CaseNumbers.subscribe(x => console.log(x.length));
+    this.CaseNumbers = new Array<CaseNumbers>();
+    this.hopkinsService.getByDay(moment().subtract(1, 'day')).pipe(mergeAll(), groupBy(x => x.Country))
+      .subscribe(group => {
 
+        const aggregate = new CaseNumbers();
+        group.forEach(x => {
+          aggregate.Infected += x.Infected;
+          if (!aggregate.Date) {
+            aggregate.Date = x.Date;
+          }
+        });
+        aggregate.Infected = 0;
+        aggregate.Country = group.key;
+        this.CaseNumbers.push(aggregate);
+      });
   }
 
 }
